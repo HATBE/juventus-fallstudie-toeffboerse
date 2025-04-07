8c91602be66283acc683113359eed71d1389a620
HATBE <admin@aarongensetter.ch>
2025-04-07 15:13:15 +0200

    product page

--- frontend/src/app/pages/product/product-page/product-page.component.spec.ts
+++ frontend/src/app/pages/product/product-page/product-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductPageComponent } from './product-page.component';
+import { UserProfileCardComponent } from '../../../components/user/user-profile-card/user-profile-card.component';
 
 describe('ProductPageComponent', () => {
   let component: ProductPageComponent;
@@ -8,10 +9,9 @@ describe('ProductPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductPageComponent]
-    })
-    .compileComponents();
-    
+      imports: [ProductPageComponent, UserProfileCardComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ProductPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- frontend/src/app/pages/product/product-page/product-page.component.ts
+++ frontend/src/app/pages/product/product-page/product-page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { SwissNumberFormatPipe } from '../../../pipes/swiss-number-format/swiss-number-format.pipe';
+import { UserProfileCardComponent } from '../../../components/user/user-profile-card/user-profile-card.component';
 
 @Component({
-    selector: 'app-product-page',
-    imports: [],
-    templateUrl: './product-page.component.html',
-    styleUrl: './product-page.component.css'
+  selector: 'app-product-page',
+  imports: [SwissNumberFormatPipe, UserProfileCardComponent],
+  templateUrl: './product-page.component.html',
+  styleUrl: './product-page.component.css',
 })
-export class ProductPageComponent {
-
-}
+export class ProductPageComponent {}
